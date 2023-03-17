b9bc1685fc8484705fadd1c3362ab154fac8c0d9
DavidBakalov21 <davidbakalov33@gmail.com>
2023-03-17 15:04:19 +0200

    first steps

---
+++ Task3/Task3/KeyValuePair.cs
@@ -0,0 +1,14 @@
+﻿namespace Task3;
+
+public class KeyValuePair
+{
+    public string Key { get; }
+
+    public string Value { get; }
+
+    public KeyValuePair(string key, string value)
+    {
+        Key = key;
+        Value = value;
+    }
+}
---
+++ Task3/Task3/LinkedListNode.cs
@@ -0,0 +1,14 @@
+﻿namespace Task3;
+
+public class LinkedListNode
+{
+    public KeyValuePair Pair { get; }
+        
+    public LinkedListNode Next { get; set; }
+
+    public LinkedListNode(KeyValuePair pair, LinkedListNode next = null)
+    {
+        Pair = pair;
+        Next = next;
+    }
+}
